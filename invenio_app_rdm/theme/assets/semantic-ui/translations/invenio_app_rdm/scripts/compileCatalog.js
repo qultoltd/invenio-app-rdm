63fae4e69067febdbad959875daf0c99030bd98f
Horváth-Gyurcsik Dóra <dgyurcsik@monguz.hu>
2025-05-07 11:01:18 +0200

    SZTERRF-34 use every po file for translation

--- invenio_app_rdm/theme/assets/semantic-ui/translations/invenio_app_rdm/scripts/compileCatalog.js
+++ invenio_app_rdm/theme/assets/semantic-ui/translations/invenio_app_rdm/scripts/compileCatalog.js
@@ -3,8 +3,9 @@
 //
 // Invenio-app-rdm is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
+const path = require("path");
 
-const { readFileSync, writeFileSync } = require("fs");
+const { readdirSync, readFileSync, writeFileSync } = require("fs");
 const { gettextToI18next } = require("i18next-conv");
 
 const PACKAGE_JSON_BASE_PATH = "./";
@@ -16,25 +17,29 @@ const options = {
   /* you options here */
 };
 
-function save(target) {
-  return (result) => {
-    writeFileSync(target, result);
-  };
+function compileTranslationsForLanguage(lang) {
+
+  const langDir = `${PACKAGE_JSON_BASE_PATH}messages/${lang}`;
+  
+  const poFiles = readdirSync(langDir).filter(file => file.endsWith(".po"));
+  
+  const promises = poFiles.map(poFile => {
+    const filePath = path.join(langDir, poFile);
+    return gettextToI18next(lang, readFileSync(filePath), options);
+  });
+
+  Promise.all(promises).then(translations => {
+    const combined = translations.reduce((result, current) => ({...result, ...JSON.parse(current)}), {});
+    const outputPath = `${langDir}/translations.json`;
+    writeFileSync(outputPath, JSON.stringify(combined, null, 2));
+  });
 }
 
 if ("lang" === process.argv[2]) {
   const lang = process.argv[3];
-  gettextToI18next(
-    lang,
-    readFileSync(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/messages.po`),
-    options
-  ).then(save(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/translations.json`));
+  compileTranslationsForLanguage(lang)
 } else {
   for (const lang of languages) {
-    gettextToI18next(
-      lang,
-      readFileSync(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/messages.po`),
-      options
-    ).then(save(`${PACKAGE_JSON_BASE_PATH}messages/${lang}/translations.json`));
+    compileTranslationsForLanguage(lang)
   }
 }
